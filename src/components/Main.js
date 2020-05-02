36e626ebd7db4c7117e3a5e9a347f83de4761603
ASHISH KUMAR <47477440+asfafasa@users.noreply.github.com>
2020-05-03 03:58:02 +0530

    status

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,14 +4,14 @@ import Home from './content/Home';
 import AboutPage from './content/AboutPage';
 import ContactPage from './content/ContactPage';
 
-const Main=()=> (
-    <Switch>
-        <Route exact path="/" component={Home} />
-        
-        <Route path="/about" component={AboutPage} />
-        
-        <Route path="/contact" component={ContactPage} />
-    </Switch>
-)
+const Main = () => (
+  <Switch>
+    <Route path='/' exact component={Home} />
+
+    <Route path='/about' component={AboutPage} />
+
+    <Route path='/contact' component={ContactPage} />
+  </Switch>
+);
 
 export default Main;
